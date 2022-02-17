a519b116641331fb1c49aa8cdc2e3ffdffc18203
clearlythuydoan <clearlythuydoan@gmail.com>
2022-02-16 21:36:35 -0500

    Index files

---
+++ client/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Button from './Button';
+// If you want to run the final button code, uncomment the line below and comment out the line above:
+// import Button from './Final-Button';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Button />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
